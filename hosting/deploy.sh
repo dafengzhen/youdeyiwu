739e1c8922e91b2d05c83105c8309b9c8fecc692
dafengzhen <dafengzhen@aliyun.com>
2024-03-18 15:44:53 +0800

    update deploy.sh

--- hosting/deploy.sh
+++ hosting/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-mkdir -p youdeyiwu-hosting && cd youdeyiwu-hosting || exit
 curl -fsSL https://raw.githubusercontent.com/dafengzhen/youdeyiwu/main/hosting/.env
 curl -fsSL https://raw.githubusercontent.com/dafengzhen/youdeyiwu/main/hosting/docker-compose.yml
 docker compose up -d
