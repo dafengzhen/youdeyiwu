700166b0443465a44903729fdd2ee25c9abeb910
dafengzhen <dafengzhen@aliyun.com>
2024-03-18 15:55:01 +0800

    update deploy.sh

--- hosting/deploy.sh
+++ hosting/deploy.sh
@@ -2,4 +2,5 @@
 
 curl -fsSL -o .env https://raw.githubusercontent.com/dafengzhen/youdeyiwu/main/hosting/.env
 curl -fsSL -o docker-compose.yml https://raw.githubusercontent.com/dafengzhen/youdeyiwu/main/hosting/docker-compose.yml
+curl -fsSL -o .env https://raw.githubusercontent.com/dafengzhen/youdeyiwu/main/hosting/README.md
 docker compose up -d
