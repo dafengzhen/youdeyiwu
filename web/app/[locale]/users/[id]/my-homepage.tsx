8268f70b9debf66a7755ba251c0f6401bd7a084a
dafengzhen <dafengzhen@aliyun.com>
2024-04-23 15:15:24 +0800

    The user's personal profile displays the date of joining

--- web/app/[locale]/users/[id]/my-homepage.tsx
+++ web/app/[locale]/users/[id]/my-homepage.tsx
@@ -3,7 +3,11 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { useState } from 'react';
 import type { IUserDetails } from '@/app/[locale]/interfaces/users';
-import { getUserAlias, isHttpOrHttps } from '@/app/[locale]/common/client';
+import {
+  formatCurrentDateTime,
+  getUserAlias,
+  isHttpOrHttps,
+} from '@/app/[locale]/common/client';
 import { useTranslations } from 'next-intl';
 import { BLUR_DATA_URL } from '@/app/[locale]/constants';
 
@@ -34,24 +38,34 @@ export default function MyHomepage({
       onClick={onClickCard}
     >
       <div className="card-body">
-        <div className="d-flex gap-3">
-          <Image
-            className="rounded-circle object-fit-contain"
-            src={isHttpOrHttps(avatar) ? avatar! : '/assets/avatar.png'}
-            alt="avatar"
-            width={80}
-            height={80}
-            placeholder="blur"
-            blurDataURL={BLUR_DATA_URL}
-          />
-          <div className="flex-grow-1 d-flex flex-column justify-content-around">
-            <div className="fw-bold user-select-all">
-              {`${getUserAlias(details)} (ID. ${details.id})`}
+        <div className="d-flex flex-column gap-4">
+          <div className="d-flex gap-3">
+            <Image
+              className="rounded-circle object-fit-contain"
+              src={isHttpOrHttps(avatar) ? avatar! : '/assets/avatar.png'}
+              alt="avatar"
+              width={80}
+              height={80}
+              placeholder="blur"
+              blurDataURL={BLUR_DATA_URL}
+            />
+            <div className="flex-grow-1 d-flex flex-column justify-content-around">
+              <div className="fw-bold user-select-all">
+                {`${getUserAlias(details)} (ID. ${details.id})`}
+              </div>
+              <div className="text-secondary-emphasis">
+                {details.oneSentence
+                  ? details.oneSentence
+                  : t('common.heDidntLeaveBehindASingleWord')}
+              </div>
             </div>
-            <div className="text-secondary-emphasis">
-              {details.oneSentence
-                ? details.oneSentence
-                : t('common.heDidntLeaveBehindASingleWord')}
+          </div>
+          <div>
+            <div className="ms-1 text-secondary-emphasis">
+              <i className="bi bi-calendar2-event me-2"></i>
+              <time dateTime={details.createdOn}>
+                {`${t('common.joiningDate')}: ${formatCurrentDateTime(details.createdOn)}`}
+              </time>
             </div>
           </div>
         </div>
