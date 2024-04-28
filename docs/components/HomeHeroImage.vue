b7963c1fa974da9c06010d52d8b2f6a6549cdd36
dafengzhen <dafengzhen@aliyun.com>
2024-04-28 15:03:09 +0800

    Add click effect to the preview images on the document homepage

--- docs/components/HomeHeroImage.vue
+++ docs/components/HomeHeroImage.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref } from 'vue';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { A11y, Navigation, Pagination, Scrollbar } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import Viewer from 'viewerjs';
 import { withBase } from 'vitepress';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import 'viewerjs/dist/viewer.css';
 
+let viewerRef = ref<Viewer>();
 const isDark = ref(false);
 let darkEl;
 
@@ -15,10 +18,24 @@ function vPSwitchAppearanceFn() {
   isDark.value = !isDark.value;
 }
 
+function onClickImage() {
+  viewerRef.value?.show();
+}
+
 onMounted(() => {
   isDark.value = document.documentElement.classList.contains('dark');
   darkEl = document.querySelector('.VPSwitchAppearance');
   darkEl?.addEventListener('click', vPSwitchAppearanceFn);
+
+  viewerRef.value = new Viewer(document.getElementById('images'), {
+    toolbar: false,
+    navbar: false,
+    title: false,
+    initialCoverage: 1,
+    viewed() {
+      viewerRef.value.zoomTo(1);
+    },
+  });
 });
 
 onUnmounted(() => {
@@ -44,7 +61,7 @@ const modules = [Navigation, Pagination, Scrollbar, A11y];
 </script>
 
 <template>
-  <div class="text-center shadow user-select-none">
+  <div id="images" class="text-center shadow user-select-none">
     <swiper
       :modules="modules"
       navigation
@@ -55,14 +72,18 @@ const modules = [Navigation, Pagination, Scrollbar, A11y];
       <swiper-slide v-for="(image, i) in images" :key="i">
         <img
           v-if="isDark"
-          class="img-fluid"
+          v-on:click="onClickImage"
+          class="image img-fluid"
+          style="cursor: zoom-in"
           :src="withBase(`/images/${image}-dark.png`)"
           alt="index"
         />
 
         <img
           v-else
-          class="img-fluid"
+          v-on:click="onClickImage"
+          class="image img-fluid"
+          style="cursor: zoom-in"
           :src="withBase(`/images/${image}.png`)"
           alt="index"
         />
