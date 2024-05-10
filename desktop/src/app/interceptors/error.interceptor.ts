14ba2d41460f1ae4f90ab65b13c5ef885986deac
dafengzhen <dafengzhen@aliyun.com>
2024-05-10 18:01:05 +0800

    Desktop: Update dependencies

--- desktop/src/app/interceptors/error.interceptor.ts
+++ desktop/src/app/interceptors/error.interceptor.ts
@@ -1,9 +1,21 @@
 import { HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { inject } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { TK } from '@/src/app/constants';
 
 export function errorInterceptor(
   req: HttpRequest<unknown>,
   next: HttpHandlerFn,
 ) {
-  return next(req).pipe(catchError((err) => throwError(() => err)));
+  const cookieService = inject(CookieService);
+  return next(req).pipe(
+    catchError((err) => {
+      if (err.status === 401) {
+        cookieService.delete(TK);
+      }
+
+      return throwError(() => err);
+    }),
+  );
 }
